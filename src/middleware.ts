05df897a469945af253c2d9f6b5561fe338cccc6
Pratyay Pandey <p.pandey@berkeley.edu>
2025-08-10 14:22:43 -0700

    Fix Clerk v6 compatibility issues: update client usage and middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/", "/api/images"],
-  ignoredRoutes: ["/api/images"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
